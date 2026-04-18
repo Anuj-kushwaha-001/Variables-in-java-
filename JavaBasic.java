a883b2f6923b1a2f3dd3a180133feecd1a8dca20
Anuj kushwaha <Anujmourya9211@gmail.com>
2026-04-18 14:47:00 +0530

    first commit

---
+++ JavaBasic.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+// Average of 3 numbers
+
+// public class JavaBasic {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//        int  A = sc.nextInt();
+//        int B = sc.nextInt();
+//        int C = sc.nextInt();
+//        int average = (A + B + C) / 3;
+//        System.out.println("Average: " + average);
+
+//     }
+// }
+
+// Area of square
+
+public class JavaBasic {
+
+  public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        int side = sc.nextInt();
+        int area = side * side;
+        System.out.println("Area of square: " + area);
+    }
+}
