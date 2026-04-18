70d25749f364f5bf516c33440f031f7e8612f56d
Anuj kushwaha <Anujmourya9211@gmail.com>
2026-04-18 15:30:23 +0530

    Add JavaCalculator and restore JavaBasic source; remove compiled class from tracking

---
+++ JavaCalculator.java
@@ -0,0 +1,41 @@
+﻿import java.util.*;
+
+public class JavaCalculator {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter a : ");
+       int a = sc.nextInt();
+       System.out.print("Enter b : ");
+       int b = sc.nextInt();
+       System.out.print("Enter operator : ");
+       char operator = sc.next().charAt(0);
+
+       switch(operator) {
+           case '+':
+               System.out.println(a + b);
+               break;
+           case '-':
+               System.out.println(a - b);
+               break;
+           case '*':
+               System.out.println(a * b);
+               break;
+           case '/':
+               if (b != 0) {
+                   System.out.println(a / b);
+               } else {
+                   System.out.println("Cannot divide by zero");
+               }
+               break;
+           case '%':
+               if (b != 0) {
+                   System.out.println(a % b);
+               } else {
+                   System.out.println("Cannot divide by zero");
+               }
+               break;
+           default:
+               System.out.println("Invalid operator");
+       }
+    }
+}
